c261770c00daf2d35e2c226cec607beeaefbb02a
코딩배우는kikiSushi <165127332+kikisushi@users.noreply.github.com>
2024-05-19 21:16:07 +0900

    초기 설정

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT;
 
 app.get('/', (req, res) => {
   return res.json(`노드몬은 자동으로 서버를 열고 닫습니다.`);
